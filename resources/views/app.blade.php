06ebf72fc6b51e4951af9d1ef81e1fc395d32bed
Emanuel Alenius <emanuel.alenius@gmail.com>
2016-12-31 00:51:12 +0000

    Modified meta tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
 	<title>Collaborate</title>
 
 	<!-- Meta tags -->
-	<meta name="viewport" content="user-scalable = yes">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<!-- CSS -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.10.0/css/bootstrap-select.min.css">
